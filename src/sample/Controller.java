dc06745d1a4b12cdb48e68e249dab08d5414d697
Thomas Lewis <thomlew@gmail.com>
2020-12-21 16:50:17 +0700

    Lesson 4
    Use fx:id and Label

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,23 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 import java.util.Random;
 
 public class Controller {
+    @FXML
+    private Label myMessage;
     public void generateRandom(ActionEvent event){
         Random rand = new Random();
         int myRand = rand.nextInt(50) + 1;
-        System.out.println(Integer.toString(myRand));
+        myMessage.setText(Integer.toString(myRand));
+       // System.out.println(Integer.toString(myRand));
     }
+
+    public void exitA(ActionEvent event){
+        System.exit(0);
+    }
+
 }
