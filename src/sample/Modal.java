fdfa4a6e179fbf75b961ec52e7dc039f4afe5969
Le Tho <thomlew1012@gmail.com>
2020-12-23 02:51:25 +0700

    Calculating in Modal

--- src/sample/Modal.java
+++ src/sample/Modal.java
@@ -2,8 +2,19 @@ package sample;
 
 public class Modal {
     public float calculate(long numb1, long numb2, String operator){
-
-
-        return 0;
+        switch(operator){
+            case "+":
+                return numb1 + numb2;
+            case "-":
+                return numb1 - numb2;
+            case "x":
+                return numb1*numb2;
+            case "/":
+                if(numb2 == 0)
+                    return 0;
+                return numb1/numb2;
+            default:
+                return 0;
+        }
     }
 }
